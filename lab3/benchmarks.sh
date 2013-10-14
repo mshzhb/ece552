e796be72c9a90f3af989d271a29c34ededa69a38
Freddy Chen <freddykychen@gmail.com>
2013-10-13 23:28:51 -0400

    benchmarks.sh

---
+++ lab3/benchmarks.sh
@@ -0,0 +1,3 @@
+sim-outorder -max:inst 50000000 -issue:inorder true -redir:sim gcc.out ./gcc.eio
+sim-outorder -max:inst 50000000 -issue:inorder true -redir:sim go.out ./go.eio
+sim-outorder -max:inst 50000000 -issue:inorder true -redir:sim compress.out ./compress.eio
