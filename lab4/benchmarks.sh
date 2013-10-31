57133d7923f4f39b70db0d0cb45efaace069d673
Freddy Chen <freddykychen@gmail.com>
2013-10-31 09:57:46 -0400

    added benchmark.sh

---
+++ lab4/benchmarks.sh
@@ -0,0 +1,3 @@
+sim-safe -max:inst 1000000 -redir:sim gcc.sim /cad2/ece552f/benchmarks/gcc.eio
+sim-safe -max:inst 1000000 -redir:sim go.sim /cad2/ece552f/benchmarks/go.eio
+sim-safe -max:inst 1000000 -redir:sim compress.sim /cad2/ece552f/benchmarks/compress.eio
