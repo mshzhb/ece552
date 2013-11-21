0867e7bff8ff6b044aa64c55ec1ab5f2fbdbf9a4
Freddy Chen <freddykychen@gmail.com>
2013-11-20 19:26:44 -0500

    rename state

--- lab5/cache.c
+++ lab5/cache.c
@@ -525,18 +525,18 @@ void open_ended_prefetcher(struct cache_t *cp, md_addr_t addr) {
 	;
 }
 
-enum rpt_state_t { Init, Transient, Steady, NoPrediction };
+enum rpt_state_t { Initial, Transient, Steady, NoPrediction };
 enum rpt_state_t get_next_rpt_state(enum rpt_state_t state, int condition) {
   enum rpt_state_t next_rpt_state;
   switch (state) {
-    case Init:
+    case Initial:
       next_rpt_state = condition ? Steady : Transient;
       break;
     case Transient:
       next_rpt_state = condition ? Steady : NoPrediction;
       break;
     case Steady:
-      next_rpt_state = condition ? Steady : Init;
+      next_rpt_state = condition ? Steady : Initial;
       break;
     case NoPrediction:
       next_rpt_state = condition ? Transient : NoPrediction;
