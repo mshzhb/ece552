8aa75938d81f24ebf4e64e608fa36f636f04eb63
Freddy Chen <freddykychen@gmail.com>
2013-11-22 11:19:09 -0500

    mbq1

--- lab5/mbq1.c
+++ lab5/mbq1.c
@@ -1,5 +1,28 @@
 #include <stdio.h>
 
+// <name>:<nsets>:<bsize>:<assoc>:<repl>:<pref>
+#define NSETS 64
+#define BSIZE 64
+struct foo {
+  char a[BSIZE];
+  char b[BSIZE];
+};
+
+
 int main(void) {
+  struct foo arr[NSETS];
+  int trash_sum = 0;
+  int random_i = 23;
+  int i;
+  for (i = 0; i < NSETS; i++) {
+    // Good for nextline prefetch
+    trash_sum += arr[i].a[random_i];
+    trash_sum += arr[i].b[random_i];
+
+    // Bad for nextline prefetch
+    //trash_sum += arr[i].a[random_i];
+    //trash_sum += arr[i].b[random_i];
+  }
+  printf("trash_sum %d\n", trash_sum);
   return 0;
 }
