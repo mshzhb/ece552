20266f82652bc11534451878ed262e4604675b61
Freddy Chen <freddykychen@gmail.com>
2013-11-22 14:36:15 -0500

    modified scripts for getting data

--- lab5/sim.sh
+++ lab5/sim.sh
@@ -3,22 +3,23 @@
 rm results.txt
 touch results.txt
 
-for prefetch_type in nextline stride open
+for prefetch_type in lru lru-nextline lru-stride lru-open
 do
   echo "********************" >> results.txt
   echo "${prefetch_type}" >> results.txt
   echo "********************" >> results.txt
   echo "" >> results.txt
 
-  for prog in gcc go compress
+  for prog in compress #gcc go compress
   do
     echo "Simulating ${prefetch_type} ${prog}..."
-    ./sim-cache -config cache-config/cache-lru-$prefetch_type.cfg -redir:sim $prog.sim -redir:prog trash.txt /cad2/ece552f/benchmarks/$prog.eio
+    ./sim-cache -config cache-config/cache-$prefetch_type.cfg -redir:sim $prog.sim -redir:prog trash.txt /cad2/ece552f/benchmarks/$prog.eio
 
     echo "${prog} results" >> results.txt
     echo "====================" >> results.txt
     echo "" >> results.txt
 
+    grep ul2.miss_rate $prog.sim >> results.txt
     grep dl1.miss_rate $prog.sim >> results.txt
     grep dl1.repl_rate $prog.sim >> results.txt
     grep dl1.wb_rate $prog.sim >> results.txt
