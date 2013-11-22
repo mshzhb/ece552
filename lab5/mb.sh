2f585d750321007109a52a43ff0c486db507dfc5
Freddy Chen <freddykychen@gmail.com>
2013-11-22 11:23:11 -0500

    update mb script

--- lab5/mb.sh
+++ lab5/mb.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-printf "" > mb_results.txt
-
 for mb in mbq1 #mbq2 mbq6
 do
   echo "Compiling ${mb}"
@@ -14,27 +12,30 @@ do
   # Disassemble code
   #/cad2/ece552f/compiler/bin/ssbig-na-sstrix-objdump -x -d -l $mb
 
+  # Clear results
+  printf "" > ${mb}_results.txt
+
   for prefetch_type in nextline stride open
   do
     echo "Simulating ${prefetch_type} for ${mb}..."
     ./sim-cache -config cache-config/cache-lru-$prefetch_type.cfg -redir:sim $mb.sim -redir:prog trash.txt $mb
 
-    echo "${mb} ${prefetch_type} results" >> mb_results.txt
-    echo "====================" >> mb_results.txt
-    echo "" >> mb_results.txt
+    echo "${mb} ${prefetch_type} results" >> ${mb}_results.txt
+    echo "====================" >> ${mb}_results.txt
+    echo "" >> ${mb}_results.txt
 
-    grep dl1.accesses $mb.sim >> mb_results.txt
-    grep dl1.hits $mb.sim >> mb_results.txt
-    grep dl1.misses $mb.sim >> mb_results.txt
-    grep dl1.replacements $mb.sim >> mb_results.txt
-    grep dl1.miss_rate $mb.sim >> mb_results.txt
-    grep dl1.repl_rate $mb.sim >> mb_results.txt
-    grep dl1.wb_rate $mb.sim >> mb_results.txt
-    grep dl1.inv_rate $mb.sim >> mb_results.txt
-    grep dl1.prefetch_accesses $mb.sim >> mb_results.txt
-    grep dl1.prefetch_hits $mb.sim >> mb_results.txt
-    grep dl1.prefetch_misses $mb.sim >> mb_results.txt
+    grep dl1.accesses $mb.sim >> ${mb}_results.txt
+    grep dl1.hits $mb.sim >> ${mb}_results.txt
+    grep dl1.misses $mb.sim >> ${mb}_results.txt
+    grep dl1.replacements $mb.sim >> ${mb}_results.txt
+    grep dl1.miss_rate $mb.sim >> ${mb}_results.txt
+    grep dl1.repl_rate $mb.sim >> ${mb}_results.txt
+    grep dl1.wb_rate $mb.sim >> ${mb}_results.txt
+    grep dl1.inv_rate $mb.sim >> ${mb}_results.txt
+    grep dl1.prefetch_accesses $mb.sim >> ${mb}_results.txt
+    grep dl1.prefetch_hits $mb.sim >> ${mb}_results.txt
+    grep dl1.prefetch_misses $mb.sim >> ${mb}_results.txt
 
-    echo "" >> mb_results.txt
+    echo "" >> ${mb}_results.txt
   done
 done
