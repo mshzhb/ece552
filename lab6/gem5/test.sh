a4fd25556a79943fc9212b83d6934d28a689a5ad
McFlurriez <anthony.alayo@gmail.com>
2013-12-04 00:10:56 -0500

    updated test script with more flags

--- lab6/gem5/test.sh
+++ lab6/gem5/test.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-./build/ALPHA/gem5.opt --debug-flags=ProtocolTrace ./configs/example/ruby_random_test.py
+./build/ALPHA/gem5.opt --debug-flags=ProtocolTrace,RubySlicc,RubyQueue ./configs/example/ruby_random_test.py
